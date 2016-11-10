fa9bda53306cc2eabb28f2c880a00a9f8fabf206
Дмитрий Рой < @dmitriyroy>
2016-11-10 17:44:39 +0200

    Работа с контроллерами

--- src/main/java/dmroy/luxoft/client/been/Line.java
+++ src/main/java/dmroy/luxoft/client/been/Line.java
@@ -1,6 +1,5 @@
 package dmroy.luxoft.client.been;
 
-import java.io.Serializable;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
